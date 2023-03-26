003afbf8332de68265ada396f18e94506e51d7bb
guseynovagid <guseynowww@inbox.ru>
2023-03-26 05:17:46 +0300

    Update main options

--- js/script.js
+++ js/script.js
@@ -302,52 +302,222 @@ window.addEventListener('DOMContentLoaded', function () {
     // Slider
 
     const slides = document.querySelectorAll('.offer__slide'),
-          prev = document.querySelector('.offer__slider-prev'),
-          next = document.querySelector('.offer__slider-next'),
-          total = document.querySelector('#total'),
-          current = document.querySelector('#current')
+        slider = document.querySelector('.offer__slider'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width
 
-
-
-    let slideIndex = 1;
-    showSlides(slideIndex)
+    let slideIndex = 1,
+        offset = 0
 
     if (slides.length < 10) {
         total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`
     } else {
         total.textContent = slides.length;
+        current.textContent = slideIndex
     }
 
-    function showSlides(n) {
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
+    slidesField.style.cssText = `
+        display: flex;
+        transition: 0.5s all;
+        width: ${100 * slides.length}%;
+    `
 
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
+    slidesWrapper.style.overflow = 'hidden';
 
-        slides.forEach(slide => slide.style.display = 'none')
-
-        slides[slideIndex - 1].style.display = 'block'
-
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
-
-    }
-
-    function changeSlideIndex(n) {
-        showSlides(slideIndex += n)
-    }
-
-    prev.addEventListener('click', () => {
-        changeSlideIndex(-1)
+    slides.forEach(slide => {
+        slide.style.width = width
     })
+
+    slider.style.position = 'relative'
+
+    const indicators = document.createElement('ol')
+    let dots = []
+    indicators.classList.add('carousel-indicators')
+
+    slider.append(indicators)
+
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '')
+    }
 
     next.addEventListener('click', () => {
-        changeSlideIndex(1)
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
+            offset = 0
+            slideIndex = 1
+            current.textContent = `0${slideIndex}`
+        } else {
+            offset += deleteNotDigits(width)
+            slideIndex++
+            current.textContent = `0${slideIndex}`
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`
+
+        dotsActivity()
     })
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = deleteNotDigits(width) * (slides.length - 1)
+            slideIndex = slides.length
+            current.textContent = `0${slideIndex}`
+        } else {
+            offset -= deleteNotDigits(width)
+            slideIndex--
+            current.textContent = `0${slideIndex}`
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`
+
+        dotsActivity()
+    })
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li')
+        dot.setAttribute('data-slide-to', i + 1)
+        dot.classList.add('dot')
+        indicators.append(dot)
+        dots.push(dot)
+
+        if (i == 0) {
+            dots[i].style.opacity = '1'
+        }
+
+        dots.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                const slideTo = e.target.getAttribute('data-slide-to')
+
+                slideIndex = slideTo
+                offset = deleteNotDigits(width) * (slideTo - 1)
+
+                slidesField.style.transform = `translateX(-${offset}px)`
+
+                current.textContent = `0${slideIndex}`
+
+                dotsActivity()
+            })
+        })
+    }
+
+    function dotsActivity() {
+        dots.forEach(i => i.style.opacity = '.5')
+        dots[slideIndex - 1].style.opacity = '1'
+    }
+
+    // Calculator
+
+    const result = document.querySelector('.calculating__result span')
+    let sex, ratio, weight, height, age
+
+    function calcTotal() {
+        if (!sex || !weight || !height || !age || !ratio) {
+            result.textContent = '_____'
+            return
+        }
+
+        if (sex == 'female') {
+            result.textContent = Math.round(447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age) * ratio)
+        } else if (sex == 'male') {
+            result.textContent = Math.round(88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age) * ratio)
+        }
+    }
+
+    calcTotal()
+
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(selector)
+
+        elements.forEach(i => {
+            i.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio')
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'))
+                } else {
+                    sex = e.target.getAttribute('id')
+                    localStorage.setItem('sex', e.target.getAttribute('id'))
+                }
+
+                elements.forEach(item => item.classList.remove(activeClass))
+                e.target.classList.add(activeClass)
+
+                calcTotal()
+            })
+        })
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio')
+    } else {
+        ratio = 1.375
+        localStorage.setItem('ratio', ratio)
+    }
+
+
+    if (localStorage.getItem('sex')) {
+        ratio = localStorage.getItem('sex')
+    } else {
+        sex = 'female'
+        localStorage.setItem('sex', sex)
+    }
+
+
+    getStaticInformation('#gender div', 'calculating__choose-item_active')
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active')
+
+
+    function getInputInformation(selector) {
+        const input = document.querySelector(selector)
+
+        input.addEventListener('input', () => {
+
+            if (input.value.match(/\D/g)) {
+                input.style.border = '1px solid red'
+            } else {
+                input.style.border = 'none'
+            }
+
+            switch (input.getAttribute('id')) {
+                case 'weight':
+                    weight = +input.value
+                    break
+                case 'height':
+                    height = +input.value
+                    break
+                case 'age':
+                    age = +input.value
+                    break
+            }
+
+            calcTotal()
+        })
+    }
+
+    function activeClassInit(selector, activeClass) {
+        const elements = document.querySelectorAll(selector)
+
+        elements.forEach(item => {
+            item.classList.remove(activeClass)
+
+            if(item.getAttribute('id') === localStorage.getItem('sex')) {
+                item.classList.add(activeClass)
+            }
+
+            if (item.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                item.classList.add(activeClass)
+            }
+        })
+    }
+
+    activeClassInit('#gender div', 'calculating__choose-item_active')
+    activeClassInit('.calculating__choose_big div', 'calculating__choose-item_active')
+
+    getInputInformation("#age")
+    getInputInformation("#weight")
+    getInputInformation("#height")
 });
